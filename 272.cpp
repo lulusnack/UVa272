2c28e0a4bae41a59b4e9d190902a33dcb3202a16
lulusnack <45396092+lulusnack@users.noreply.github.com>
2019-12-13 00:11:32 +0800

    Add files via upload

---
+++ 272.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string input;
+	bool judge = true;
+	while (getline(cin,input))
+	{	
+		int len = input.length();
+		for (int i = 0; i < len; i++)
+		{
+			if (input[i] == '"')
+			{
+				if (judge == true)
+				{
+					cout << "``";
+					judge = false;
+				}
+				else
+				{
+					cout << "''";
+					judge = true;
+				}
+			}
+			else
+				cout << input[i];
+		}
+		cout << endl;
+	}
+	return 0;
+}
